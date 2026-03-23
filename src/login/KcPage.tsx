4ba9a3ea2290403e249c4f88d24bf8cef5c968a5
Julie <julie.buma@mobiliteitsfabriek.nl>
2026-03-23 14:32:33 +0100

    Adds tokens + input styling

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -4,37 +4,45 @@ import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
-const UserProfileFormFields = lazy(
-    () => import("keycloakify/login/UserProfileFormFields")
-);
+import "./styles/tokens/reisbalans.css";
+import "./styles/tokens/main.css";
+import "./main.css";
+
+const UserProfileFormFields = lazy(() => import("keycloakify/login/UserProfileFormFields"));
 
 const doMakeUserConfirmPassword = true;
 
 export default function KcPage(props: { kcContext: KcContext }) {
-    const { kcContext } = props;
+  const { kcContext } = props;
 
-    const { i18n } = useI18n({ kcContext });
+  const { i18n } = useI18n({ kcContext });
 
-    return (
-        <Suspense>
-            {(() => {
-                switch (kcContext.pageId) {
-                    default:
-                        return (
-                            <DefaultPage
-                                kcContext={kcContext}
-                                i18n={i18n}
-                                classes={classes}
-                                Template={Template}
-                                doUseDefaultCss={true}
-                                UserProfileFormFields={UserProfileFormFields}
-                                doMakeUserConfirmPassword={doMakeUserConfirmPassword}
-                            />
-                        );
-                }
-            })()}
-        </Suspense>
-    );
+  return (
+    <Suspense>
+      {(() => {
+        switch (kcContext.pageId) {
+          default:
+            return (
+              <DefaultPage
+                kcContext={kcContext}
+                i18n={i18n}
+                classes={classes}
+                Template={Template}
+                doUseDefaultCss={true}
+                UserProfileFormFields={UserProfileFormFields}
+                doMakeUserConfirmPassword={doMakeUserConfirmPassword}
+              />
+            );
+        }
+      })()}
+    </Suspense>
+  );
 }
 
-const classes = {} satisfies { [key in ClassKey]?: string };
+const classes = {
+  kcInputClass: "",
+  kcInputGroup: "",
+  kcFormPasswordVisibilityButtonClass: "",
+  kcCheckClass: "",
+  kcCheckboxInputClass: "",
+} satisfies { [key in ClassKey]?: string };
