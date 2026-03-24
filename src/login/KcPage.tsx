c2b3a2ec250348326658ba3548b4a9c3c7612de6
Julie <julie.buma@mobiliteitsfabriek.nl>
2026-03-24 15:55:09 +0100

    DEV-1291 Eject Template page

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -3,13 +3,11 @@ import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
-import Template from "keycloakify/login/Template";
+import Template from "./Template";
 import "./styles/tokens/main.css";
 import "./styles/main.css";
 
 const UserProfileFormFields = lazy(() => import("keycloakify/login/UserProfileFormFields"));
-const LoginPassword = lazy(() => import("./pages/LoginPassword"));
-const LoginUsername = lazy(() => import("./pages/LoginUsername"));
 
 const doMakeUserConfirmPassword = true;
 
@@ -19,6 +17,7 @@ export default function KcPage(props: { kcContext: KcContext }) {
 
   const { i18n } = useI18n({ kcContext });
 
+  // Dynamically import theme tokens based on themeName
   useEffect(() => {
     const loadTheme = async () => {
       await import(`./styles/tokens/${themeName ?? "reisbalans"}.css`);
@@ -31,10 +30,6 @@ export default function KcPage(props: { kcContext: KcContext }) {
     <Suspense>
       {(() => {
         switch (kcContext.pageId) {
-          case "login-password.ftl":
-            return <LoginPassword {...{ kcContext, i18n, classes }} Template={Template} doUseDefaultCss={true} />;
-          case "login-username.ftl":
-            return <LoginUsername {...{ kcContext, i18n, classes }} Template={Template} doUseDefaultCss={true} />;
           default:
             return (
               <DefaultPage
--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -3,7 +3,27 @@ import { i18nBuilder } from "keycloakify/login";
 import type { ThemeName } from "../kc.gen";
 
 /** @see: https://docs.keycloakify.dev/features/i18n */
-const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+const { useI18n, ofTypeI18n } = i18nBuilder
+  .withThemeName<ThemeName>()
+  .withCustomTranslations({
+    en: {
+      loginProblems: "Having trouble logging in?",
+      contactIfProblem1: "Feel free to",
+      contact: "contact",
+      contactIfProblem2: "us",
+      atPlatform: "at {0}",
+    },
+    nl: {
+      // There's no way to dynamically inject html elements into a translation, only strings
+      // So we have split it up this way
+      loginProblems: "Problemen met inloggen?",
+      contactIfProblem1: "Neem gerust",
+      contact: "contact",
+      contactIfProblem2: "op",
+      atPlatform: "bij {0}",
+    },
+  })
+  .build();
 
 type I18n = typeof ofTypeI18n;
 
