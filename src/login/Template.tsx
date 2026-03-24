3056f34a96f581422502fb6bd3f93d4d67b51d51
Julie <julie.buma@mobiliteitsfabriek.nl>
2026-03-24 16:58:06 +0100

    DEV-1291 Layout styling

--- src/login/Template.tsx
+++ src/login/Template.tsx
@@ -56,13 +56,13 @@ export default function Template(props: TemplateProps<KcContext, I18n>) {
 
   return (
     <div className={kcClsx("kcLoginClass")}>
-      <div id="kc-header" className={kcClsx("kcHeaderClass")}>
-        <div id="kc-header-wrapper" className={kcClsx("kcHeaderWrapperClass")}>
-          <PlatformLogo platformName={themeName} />
-        </div>
-      </div>
       <div className={kcClsx("kcFormCardClass")}>
         <header className={kcClsx("kcFormHeaderClass")}>
+          <div id="kc-header" className={kcClsx("kcHeaderClass")}>
+            <div id="kc-header-wrapper" className={kcClsx("kcHeaderWrapperClass")}>
+              <PlatformLogo platformName={themeName} />
+            </div>
+          </div>
           {enabledLanguages.length > 1 && (
             <div className={kcClsx("kcLocaleMainClass")} id="kc-locale">
               <div id="kc-locale-wrapper" className={kcClsx("kcLocaleWrapperClass")}>
@@ -121,69 +121,67 @@ export default function Template(props: TemplateProps<KcContext, I18n>) {
             return node;
           })()}
         </header>
-        {auth !== undefined && auth.showUsername && !auth.showResetCredentials && (
-          <div id="kc-username" className={kcClsx("kcFormGroupClass")}>
-            <a id="reset-login" href={url.loginRestartFlowUrl} aria-label={msgStr("restartLoginTooltip")}>
-              <div className="kc-login-tooltip">
-                <i className={kcClsx("kcResetFlowIcon")}></i>
-                <span className="kc-tooltip-text">{msg("restartLoginTooltip")}</span>
-              </div>
-            </a>
-            <label id="kc-attempted-username">{auth.attemptedUsername}</label>
-          </div>
-        )}
         <div id="kc-content">
-          <div id="kc-content-wrapper">
-            {/* App-initiated actions should not see warning messages about the need to complete the action during login. */}
-            {displayMessage && message !== undefined && (message.type !== "warning" || !isAppInitiatedAction) && (
-              <div
-                className={clsx(
-                  `alert-${message.type}`,
-                  kcClsx("kcAlertClass"),
-                  `pf-m-${message?.type === "error" ? "danger" : message.type}`
-                )}
-              >
-                <div className="pf-c-alert__icon">
-                  {message.type === "success" && <span className={kcClsx("kcFeedbackSuccessIcon")}></span>}
-                  {message.type === "warning" && <span className={kcClsx("kcFeedbackWarningIcon")}></span>}
-                  {message.type === "error" && <span className={kcClsx("kcFeedbackErrorIcon")}></span>}
-                  {message.type === "info" && <span className={kcClsx("kcFeedbackInfoIcon")}></span>}
+          {auth !== undefined && auth.showUsername && !auth.showResetCredentials && (
+            <div id="kc-username" className={kcClsx("kcFormGroupClass")}>
+              <a id="reset-login" href={url.loginRestartFlowUrl} aria-label={msgStr("restartLoginTooltip")}>
+                <div className="kc-login-tooltip">
+                  <i className={kcClsx("kcResetFlowIcon")}></i>
+                  <span className="kc-tooltip-text">{msg("restartLoginTooltip")}</span>
                 </div>
-                <span
-                  className={kcClsx("kcAlertTitleClass")}
-                  dangerouslySetInnerHTML={{
-                    __html: kcSanitize(message.summary),
+              </a>
+              <label id="kc-attempted-username">{auth.attemptedUsername}</label>
+            </div>
+          )}
+          {/* App-initiated actions should not see warning messages about the need to complete the action during login. */}
+          {displayMessage && message !== undefined && (message.type !== "warning" || !isAppInitiatedAction) && (
+            <div
+              className={clsx(
+                `alert-${message.type}`,
+                kcClsx("kcAlertClass"),
+                `pf-m-${message?.type === "error" ? "danger" : message.type}`
+              )}
+            >
+              <div className="pf-c-alert__icon">
+                {message.type === "success" && <span className={kcClsx("kcFeedbackSuccessIcon")}></span>}
+                {message.type === "warning" && <span className={kcClsx("kcFeedbackWarningIcon")}></span>}
+                {message.type === "error" && <span className={kcClsx("kcFeedbackErrorIcon")}></span>}
+                {message.type === "info" && <span className={kcClsx("kcFeedbackInfoIcon")}></span>}
+              </div>
+              <span
+                className={kcClsx("kcAlertTitleClass")}
+                dangerouslySetInnerHTML={{
+                  __html: kcSanitize(message.summary),
+                }}
+              />
+            </div>
+          )}
+          {children}
+          {auth !== undefined && auth.showTryAnotherWayLink && (
+            <form id="kc-select-try-another-way-form" action={url.loginAction} method="post">
+              <div className={kcClsx("kcFormGroupClass")}>
+                <input type="hidden" name="tryAnotherWay" value="on" />
+                <a
+                  href="#"
+                  id="try-another-way"
+                  onClick={() => {
+                    document.forms["kc-select-try-another-way-form" as never].requestSubmit();
+                    return false;
                   }}
-                />
+                >
+                  {msg("doTryAnotherWay")}
+                </a>
               </div>
-            )}
-            {children}
-            {auth !== undefined && auth.showTryAnotherWayLink && (
-              <form id="kc-select-try-another-way-form" action={url.loginAction} method="post">
-                <div className={kcClsx("kcFormGroupClass")}>
-                  <input type="hidden" name="tryAnotherWay" value="on" />
-                  <a
-                    href="#"
-                    id="try-another-way"
-                    onClick={() => {
-                      document.forms["kc-select-try-another-way-form" as never].requestSubmit();
-                      return false;
-                    }}
-                  >
-                    {msg("doTryAnotherWay")}
-                  </a>
-                </div>
-              </form>
-            )}
-            {socialProvidersNode}
-            {displayInfo && (
-              <div id="kc-info" className={kcClsx("kcSignUpClass")}>
-                <div id="kc-info-wrapper" className={kcClsx("kcInfoAreaWrapperClass")}>
-                  {infoNode}
-                </div>
+            </form>
+          )}
+          {socialProvidersNode}
+          {displayInfo && (
+            <div id="kc-info" className={kcClsx("kcSignUpClass")}>
+              <div id="kc-info-wrapper" className={kcClsx("kcInfoAreaWrapperClass")}>
+                {infoNode}
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       </div>
       <p className="login-trouble">
